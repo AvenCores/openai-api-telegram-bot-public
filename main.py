b7c3c87de09b4a491186ecaa23f529e8d90c4413
avencores <64781822+AvenCores@users.noreply.github.com>
2023-04-26 15:11:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import time
 import sys
 import os
 
-from botapiconfig import openaiapi, telegrambotapi, session_key, quora_token
+from botapiconfig import openaiapi, telegrambotapi, session_key, quora_token, botname, timebot, numbers
 
 openai.api_key = openaiapi
 bot = telebot.TeleBot(telegrambotapi)
@@ -28,13 +28,6 @@ last_messages_dalletwo = {}
 last_whisper = {}
 
 start_time = time.time()
-
-botname = "avencoreschatgpt_bot"
-
-timebot = "Europe/Moscow"
-
-q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,qw  = ['dhfdhdfhf.webp', 'fgjgfjfgj.webp', 'fhfdhfdh.webp', 'fjfgjurturt.webp', 'hdfhdfhdfh.webp', 'rtutrurtutru.webp', 'sticker.webp', 'sticker-animenazi.webp', 'trutrutrur.webp', 'urturturtutru.webp', 'rutrutrutrurt.webp', 'jfjfgjfgjg.webp', 'fjgfjfgjgfj.webp', 'jfgjturtur.webp', 'rtutrurtjfgj.webp', 'urturutrurt.webp', '54754745745.webp', '5568568568568.webp', '568568658568.webp', '56868568568.webp', '65865856856.webp', '65856865865.webp', '658658658.webp', '68658658658.webp', '754745745.webp', '745745745754.webp', '4354353454.webp']
-numbers = (q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,qw)
 
 
 ADMIN_USERS_FILE = "admin_list.txt"
