e038aaf00cba8dca54f7edc3efe29065415fad8e
avencores <64781822+AvenCores@users.noreply.github.com>
2023-05-01 20:32:06 +0300

    Update botapiconfig.py

--- botapiconfig.py
+++ botapiconfig.py
@@ -1,10 +1,11 @@
 telegrambotapi = ""
 openaiapi = ""
 session_key = ""
-quora_token = ""
 
 botname = "avencoreschatgpt_bot"
 timebot = "Europe/Moscow"
 
 q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,qw  = ['dhfdhdfhf.webp', 'fgjgfjfgj.webp', 'fhfdhfdh.webp', 'fjfgjurturt.webp', 'hdfhdfhdfh.webp', 'rtutrurtutru.webp', 'sticker.webp', 'sticker-animenazi.webp', 'trutrutrur.webp', 'urturturtutru.webp', 'rutrutrutrurt.webp', 'jfjfgjfgjg.webp', 'fjgfjfgjgfj.webp', 'jfgjturtur.webp', 'rtutrurtjfgj.webp', 'urturutrurt.webp', '54754745745.webp', '5568568568568.webp', '568568658568.webp', '56868568568.webp', '65865856856.webp', '65856865865.webp', '658658658.webp', '68658658658.webp', '754745745.webp', '745745745754.webp', '4354353454.webp']
 numbers = (q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m,qw)
+
+chatgpt4_test = "yes" # yes/not
